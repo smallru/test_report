b71d9c1d591ae69c913a9dbd45e2236403386630
xiaoru <zhaoxiaoru123@163.com>
2019-01-15 23:48:24 +0800

    xiugaijiemian

--- dev/main.py
+++ dev/main.py
@@ -19,29 +19,59 @@ def GenerateTestReports(path):
 
 class MainFrame(wx.Frame):
     def __init__(self):
-        super().__init__(None, -1, "自动生成测试报告", size=(600, 150))
-        self.m_BasePath = ''
+        self.system_doc_path = ''
+        self.station_doc_path = ''
+        self.station_doc_svn = ''
+        super().__init__(None, -1, "自动生成测试报告", size=(800, 500))
         panel = wx.Panel(self, -1)
-        self.ChoosePathBtn = wx.Button(panel, -1, "浏览", pos=(500, 15), size=(50, 26))
-        self.InputLineAddressText = wx.TextCtrl(panel, -1, value="请选择读取xml路径", pos=(50, 15), size=(450, 25),
-                                                style=wx.TE_READONLY)
-        self.ExecuteBtn = wx.Button(panel, -1,"请选择", pos=(240, 60), size=(120, 30))
-        self.Bind(wx.EVT_BUTTON, self.OnClick_Path, self.ChoosePathBtn)
+
+        #系统方案路径窗口
+        self.systemDocPathText = wx.StaticText(panel, label="系统方案路径：", pos=(20, 20))
+        self.systemDocPath = wx.TextCtrl(panel, -1, value="请选择系统方案路径", pos=(20, 40), size=(150, 20),
+                                         style=wx.TE_READONLY)
+        self.systemDocPathOption = wx.Button(panel, -1, "浏览", pos=(170, 40), size=(50, 20))
+        self.Bind(wx.EVT_BUTTON, self.OnClick_systemDocPath, self.systemDocPathOption)
+
+        #车站方案路径窗口
+        self.stationDocPathText = wx.StaticText(panel, label="车站方案路径：", pos=(20, 80))
+        self.stationDocPath = wx.TextCtrl(panel, -1, value="请选择车站方案路径", pos=(20, 100), size=(150, 20),
+                                         style=wx.TE_READONLY)
+        self.stationDocPathOption = wx.Button(panel, -1, "浏览", pos=(170, 100), size=(50, 20))
+        self.Bind(wx.EVT_BUTTON, self.OnClick_stationDocPath, self.stationDocPathOption)
+
+        #SVN号窗口
+        self.stationDocSvnText = wx.StaticText(panel, label="车站方案SVN号：", pos=(20, 140))
+        self.stationDocSvn = wx.TextCtrl(panel, -1, value="请选择车站方案路径", pos=(20, 140), size=(150, 20),
+                                          style=wx.TE_READONLY)
+        #self.station_doc_svn = self.stationDocSvn.GetPath()
+
+
+        self.ExecuteBtn = wx.Button(panel, -1,"请选择", pos=(600, 400), size=(100, 30))
         self.Bind(wx.EVT_BUTTON, self.OnClick_Execute, self.ExecuteBtn)
 
-    def OnClick_Path(self, event):
-        dialog = wx.DirDialog(None, "选择车站报告xml路径", style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
+    def OnClick_systemDocPath(self, event):
+        dialog = wx.DirDialog(None, "请选择系统方案路径", style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
         if dialog.ShowModal() == wx.ID_OK:
-            self.m_BasePath = dialog.GetPath()
-            self.InputLineAddressText.SetValue(self.m_BasePath)
+            self.system_doc_path = dialog.GetPath()
+            self.systemDocPath.SetValue(self.system_doc_path)
         dialog.Destroy()
-        self.ExecuteBtn.SetLabel("开始")
+        #self.ExecuteBtn.SetLabel("开始")
+
+    def OnClick_stationDocPath(self, event):
+        dialog = wx.DirDialog(None, "请选择车站方案路径", style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
+        if dialog.ShowModal() == wx.ID_OK:
+            self.station_doc_path = dialog.GetPath()
+            self.stationDocPath.SetValue(self.station_doc_path)
+        dialog.Destroy()
 
     def OnClick_Execute(self, event):
-        if self.m_BasePath == '':
+        if self.system_doc_path == '':
             return
-        GenerateTestReports(self.m_BasePath)
+        GenerateTestReports(self.system_doc_path)
         self.ExecuteBtn.SetLabel("结束")
+
+    def __str__(self):
+        return self.system_doc_path+self.station_doc_path+self.station_doc_svn
 
 
 if __name__ == '__main__':
@@ -49,3 +79,4 @@ if __name__ == '__main__':
     g_Frame = MainFrame()
     g_Frame.Show()
     app.MainLoop()
+    print(g_Frame)
--- dev/test.py
+++ dev/test.py
@@ -6,37 +6,58 @@
 @create_time=2019/1/6 15:42
 """
 import wx
+class ExamplePanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        self.quote = wx.StaticText(self, label="Your quote :", pos=(20, 30))
 
-class HelloFrame(wx.Frame):
-    """
-    A Frame that says Hello World
-    """
+        # A multiline TextCtrl - This is here to show how the events work in this program, don't pay too much attention to it
+        self.logger = wx.TextCtrl(self, pos=(300,20), size=(200,300), style=wx.TE_MULTILINE | wx.TE_READONLY)
 
-    def __init__(self, *args, **kw):
-        # ensure the parent's __init__ is called
-        super(HelloFrame, self).__init__(*args, **kw)
+        # A button
+        self.button =wx.Button(self, label="Save", pos=(200, 325))
+        self.Bind(wx.EVT_BUTTON, self.OnClick,self.button)
 
-        # create a panel in the frame
-        panel = wx.Panel(self,-1)
-        self.button = wx.Button(panel, -1, "start", pos=(150, 20))
-        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
-        self.button.SetDefault()
-        b = wx.Button(panel, -1, u"文件夹选择对话框",pos=(150, 90))
-        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        # the edit control - one line version.
+        self.lblname = wx.StaticText(self, label="Your name :", pos=(20,60))
+        self.editname = wx.TextCtrl(self, value="Enter here your name", pos=(150, 60), size=(140,-1))
+        self.Bind(wx.EVT_TEXT, self.EvtText, self.editname)
+        self.Bind(wx.EVT_CHAR, self.EvtChar, self.editname)
 
-    def OnClick(self, event):
-        self.button.SetLabel("end")
+        # the combobox Control
+        self.sampleList = ['friends', 'advertising', 'web search', 'Yellow Pages']
+        self.lblhear = wx.StaticText(self, label="How did you hear from us ?", pos=(20, 90))
+        self.edithear = wx.ComboBox(self, pos=(150, 90), size=(95, -1), choices=self.sampleList, style=wx.CB_DROPDOWN)
+        self.Bind(wx.EVT_COMBOBOX, self.EvtComboBox, self.edithear)
+        self.Bind(wx.EVT_TEXT, self.EvtText,self.edithear)
 
-    def OnButton(self, event):
-        dlg = wx.DirDialog(self, u"选择文件夹", style=wx.DD_DEFAULT_STYLE)
-        if dlg.ShowModal() == wx.ID_OK:
-            print(dlg.GetPath())  # 文件夹路径
-        dlg.Destroy()
+        # Checkbox
+        self.insure = wx.CheckBox(self, label="Do you want Insured Shipment ?", pos=(20,180))
+        self.Bind(wx.EVT_CHECKBOX, self.EvtCheckBox, self.insure)
 
-if __name__ == '__main__':
-    # When this module is run (not imported) then create the app, the
-    # frame, show it, and start the event loop.
-    app = wx.App()
-    frm = HelloFrame(None, title='Hello World 2')
-    frm.Show()
-    app.MainLoop()
+        # Radio Boxes
+        radioList = ['blue', 'red', 'yellow', 'orange', 'green', 'purple', 'navy blue', 'black', 'gray']
+        rb = wx.RadioBox(self, label="What color would you like ?", pos=(20, 210), choices=radioList,  majorDimension=3,
+                         style=wx.RA_SPECIFY_COLS)
+        self.Bind(wx.EVT_RADIOBOX, self.EvtRadioBox, rb)
+
+    def EvtRadioBox(self, event):
+        self.logger.AppendText('EvtRadioBox: %d\n' % event.GetInt())
+    def EvtComboBox(self, event):
+        self.logger.AppendText('EvtComboBox: %s\n' % event.GetString())
+    def OnClick(self,event):
+        self.logger.AppendText(" Click on object with Id %d\n" %event.GetId())
+    def EvtText(self, event):
+        self.logger.AppendText('EvtText: %s\n' % event.GetString())
+    def EvtChar(self, event):
+        self.logger.AppendText('EvtChar: %d\n' % event.GetKeyCode())
+        event.Skip()
+    def EvtCheckBox(self, event):
+        self.logger.AppendText('EvtCheckBox: %d\n' % event.Checked())
+
+
+app = wx.App(False)
+frame = wx.Frame(None)
+panel = ExamplePanel(frame)
+frame.Show()
+app.MainLoop()
